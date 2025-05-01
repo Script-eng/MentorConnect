302b39977d6acdd7c0290cd8557b795f93d21be0
salahbensarar1 <sa.bensarar@gmail.com>
2025-05-01 12:27:52 +0200

    Setup MentorHomeActivity with bottom navigation and fragment switching

--- app/src/main/java/hu/nje/mentorconnect/activities/MentorHomeActivity.java
+++ app/src/main/java/hu/nje/mentorconnect/activities/MentorHomeActivity.java
@@ -1,22 +1,54 @@
 package hu.nje.mentorconnect.activities;
 
 import android.os.Bundle;
-import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import hu.nje.mentorconnect.R;
+import hu.nje.mentorconnect.fragments.ChatFragment;
+import hu.nje.mentorconnect.fragments.DocsFragment;
+import hu.nje.mentorconnect.fragments.HomeFragment;
+import hu.nje.mentorconnect.fragments.MenteesFragment;
 
 public class MentorHomeActivity extends AppCompatActivity {
-
-    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mentor_home);
 
-        welcomeText = findViewById(R.id.welcome_text);
-        welcomeText.setText("Welcome, Mentor!");
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Load default fragment
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (id == R.id.nav_mentees) {
+                selectedFragment = new MenteesFragment();
+            } else if (id == R.id.nav_chat) {
+                selectedFragment = new ChatFragment();
+            } else if (id == R.id.nav_documents) {
+                selectedFragment = new DocsFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+
+            return true;
+        });
     }
 }
